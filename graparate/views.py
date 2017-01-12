0d2f9484c5584ac6d5fc0b6b333bd2264b7e52ef
walkup <walkuper@users.noreply.github.com>
2017-01-13 03:23:20 +0800

    Revert "1/13 更新後調整"
    This reverts commit 49487dff927e81573503e84e24927414bdace21d.

--- graparate/views.py
+++ graparate/views.py
@@ -59,25 +59,51 @@ def bankpack(request):
     for i in range(len(getjson)):
         print (getjson[i]['bankname'])
 
-def test(request):
+def test(requestion):
+    '''
+    request = urllib.request.Request("http://127.0.0.1:8000/static/java/currency_type.json")
+    response = urllib.request.urlopen(request)
+    
+    #print (response.read().decode('utf-8'))
+    
+    cashholder = response.read().decode('utf-8')
+    r = cashholder
+    '''
     rArr=[]
-    r = requests.get('http://127.0.0.1:8000/static/java/currency_type.json').json()
+    r = requests.get("http://127.0.0.1:8000/static/java/currency_type.json").json()
+    print (type(r))
     rArr.append(r)
     rr = json.dumps(rArr)
+    print ('rr type :' + str(type(rr)))
+    print ('rr len:' + str(len(rr)))
     rrr = json.loads(rr)
-    return render(request, 'test.html', {
+    print ('rrr type :' + str(type(rrr)))
+    print ('rrr len :' + str(len(rrr)))
+    return render(requestion, 'test.html', {
            'r': rrr,
            })
     #return render_to_response('test.html',locals())
+'''
+class CType(object):
+    def __init__(self, Nation, Language, Currency):
+        self = self
+        self.Nation = Nation
+        self.Language = Language
+        self.Currency = Currency
+
+def object_decoder(obj):
+    if '__type__' in obj and obj['__type__'] == 'CType':
+        return CType(obj,obj['Nation'], obj['Language'], obj['Currency'])
+    return obj
+
+json.loads('http://127.0.0.1:8000/static/java/currency_type.json', object_hook=object_decoder)
+
+print (type(CType))
+'''
 
 
 def test2(request):
-    r = requests.get(r'http://tw.rter.info/capi.php')
-    html = r.content
-    js = json.loads(html)
-    print (js['USDHKD']['Exrate'])
-    return render(request, 'test2.html',{'html': HttpResponse(html)})
-    #return render_to_response('test2.html',locals())
+    return render_to_response('test2.html',locals())
 
 base_dir = settings.BASEDIR()
 filter = base_dir + "/templates/" + ""
